0541826cddedd84b61c9bcc07975adbb62d0072a
stt <931040676@qq.com>
2018-04-27 14:30:21 +0800

    update threadpool

--- ST_server/src_code/threadpool.c
+++ ST_server/src_code/threadpool.c
@@ -25,10 +25,101 @@ int threadpool_free(st_threadpool_t *pool)
     }
     return 0;
 }
-void *threadpool_work(void *arg)
-{
 
+void *threadpool_worker(void *arg)
+{
+    if(arg==NULL)
+        return NULL;
+
+
+    st_threadpool_t *pool=(st_threadpool_t *)arg;
+    st_task_t *task;
+
+    while(1)
+    {
+        //线程池上锁
+        pthread_mutex_lock(&(pool->lock));
+
+
+         // 没有task且未停机则阻塞
+        while((pool->queue_size == 0) && !(pool->shutdown))
+            pthread_cond_wait(&(pool->cond), &(pool->lock));
+
+        // 立即停机模式、平滑停机且没有未完成任务则退出
+        if(pool->shutdown == immediate_shutdown)
+            break;
+        else if((pool->shutdown == graceful_shutdown) && (pool->queue_size == 0))
+            break;
+
+
+
+        //获取链表中第一个tast
+        task=pool->head->next;
+        if(task==NULL)
+        {
+            pthread_mutex_unlock(&(pool->lock));
+            continue;
+        }
+
+        //存在task则取走
+        pool->head->next=task->next;
+        pool->queue_size--;
+        pthread_mutex_unlock(&(pool->lock));
+
+        //设置task->func参数
+        (*(task->func))(task->arg);
+        free(task);
+    }
+    pool->started--;
+    pthread_mutex_unlock(&(pool->lock));
+    pthread_exit(NULL);
     return NULL;
+}
+
+
+//释放线程资源
+int threadpool_destroy(st_threadpool_t *pool,int graceful)
+{
+    if(pool == NULL)
+        return st_tp_invalid;
+    if(pthread_mutex_lock(&(pool->lock)) != 0)
+        return st_tp_lock_fail;
+
+    int err = 0;
+    do{
+        if(pool->shutdown){
+            err = st_tp_already_shutdown;
+            break;
+        }
+
+        pool->shutdown = (graceful) ? graceful_shutdown : immediate_shutdown;
+
+        if(pthread_cond_broadcast(&(pool->cond)) != 0){
+            err = st_tp_cond_broadcast;
+            break;
+        }
+
+        if(pthread_mutex_unlock(&(pool->lock)) != 0){
+            err = st_tp_lock_fail;
+            break;
+        }
+
+        // 回收每个线程资源
+        for(int i = 0; i < pool->thread_count; i++){
+            if(pthread_join(pool->threads[i], NULL) != 0){
+                err = st_tp_thread_fail;
+            }
+        }
+    }while(0);
+
+    if(!err){
+        pthread_mutex_destroy(&(pool->lock));
+        pthread_cond_destroy(&(pool->cond));
+        threadpool_free(pool);
+    }
+    return err;
+
+
 }
 
 st_threadpool_t* threadpool_init(int thread_num)
@@ -59,10 +150,48 @@ st_threadpool_t* threadpool_init(int thread_num)
     //创建线程 thread_num个
     for(int i=0;i<thread_num;i++)
     {
-        if(pthread_create(&(pool->threads[i]),NULL,threadpool_work,(void*)pool)!=0)
+        if(pthread_create(&(pool->threads[i]),NULL,threadpool_worker,(void*)pool)!=0)
             return NULL;
     }
 
 
     return pool;
+}
+
+
+int threadpool_add(st_threadpool_t* pool, void (*func)(void *), void* arg)
+{
+    //int rc=0;
+    int err = 0;
+    if(pool == NULL || func == NULL)
+        return -1;
+
+    if(pthread_mutex_lock(&(pool->lock)) != 0)
+        return -1;
+
+    // 已设置关机
+    if(pool->shutdown){
+        err = st_tp_already_shutdown;
+        goto out;
+    }
+
+    // 新建task并注册信息
+    st_task_t *task = (st_task_t *)malloc(sizeof(st_task_t));
+    if(task == NULL)
+        goto out;
+    task->func = func;
+    task->arg = arg;
+
+    // 新task节点在head处插入
+    task->next = pool->head->next;
+    pool->head->next = task;
+    pool->queue_size++;
+
+    pthread_cond_signal(&(pool->cond));
+
+out:
+    if(pthread_mutex_unlock(&pool->lock) != 0)
+        return -1;
+    return err;
+
 }
--- ST_server/src_code/threadpool.h
+++ ST_server/src_code/threadpool.h
@@ -23,10 +23,23 @@ typedef struct threadpool{
     st_task_t *head;          // 任务链表
     int thread_count;         // 线程数
     int queue_size;           // 任务链表长
-    int shutdown;             // 关机模式
+    int shutdown;             // 关机模式 线程池是否销毁
     int started;
 }st_threadpool_t;
 
+
+typedef enum{
+    st_tp_invalid = -1,
+    st_tp_lock_fail = -2,
+    st_tp_already_shutdown = -3,
+    st_tp_cond_broadcast = -4,
+    st_tp_thread_fail = -5,
+}st_threadpool_error_t;
+
+typedef enum{
+    immediate_shutdown = 1,
+    graceful_shutdown = 2
+}st_threadpool_sd_t;
 
 st_threadpool_t* threadpool_init(int thread_num);
 int threadpool_add(st_threadpool_t* pool, void (*func)(void *), void* arg);
